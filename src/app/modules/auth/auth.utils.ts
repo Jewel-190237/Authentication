7ddd438ad876bf7622d74c52ab3fd0fe2e2aacc7
Jewel-190237 <1902378@ku.ac.bd>
2025-08-16 22:44:19 +0600

    user created added

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -1,6 +1,12 @@
 import { z } from "zod"
-
+import { TTokenPayload } from "./auth.interface"
+import jwt from 'jsonwebtoken'
 export const checkValidEmail = (email: string) => {
    const schema = z.string().email()
    return schema.safeParse(email)
+}
+
+export const createToken = (payload: TTokenPayload, secret: string, expiresIn: string) => {
+   //@ts-ignore
+   return jwt.sign(payload, secret, { expiresIn })
 }
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,16 +1,84 @@
+import { HttpStatusCode } from "axios";
+import AppError from "../../errors/AppError";
 import { catchAsync } from "../../utils/catchAsync";
 import { OTPService } from "../otp/otp.service";
-
+import dayjs from 'dayjs';
+import { UserService } from "./user.service";
+import config from "../../config";
+import { createToken } from "../auth/auth.utils";
+import sendResponse from "../../utils/sendResponse";
 
 export class UserController {
    static createNewUser = catchAsync(async (req, res) => {
       const { body } = req.body
       let otp;
 
-      otp = OTPService.findOneByEmail({
+      otp = await OTPService.findOneByEmail({
          email: body.idetifier,
          code: body.code,
          action: body.action
       })
+
+      if (!otp) {
+         throw new AppError(
+            HttpStatusCode.BadRequest,
+            'verification failed',
+            'invalid or expire otp'
+         )
+      }
+
+      const startTime = dayjs(otp.createdAt)
+      const endTime = dayjs(Date.now());
+      const expireTimeInMinutes = endTime.diff(startTime, 'minute')
+      if (expireTimeInMinutes >= 2) {
+
+         throw new AppError(
+            400,
+            'Invalid request',
+            'OTP expired! Please try again.',
+         );
+      }
+
+      if (otp && otp.attempst > 0 && body.code === otp.code) {
+         const newUser = await UserService.createNewUser(body)
+
+         const tokenPayload: any = {
+            //@ts-ignore
+            _id: newUser._id,
+            //@ts-ignore
+            name: newUser.name,
+            //@ts-ignore
+            email: newUser.email,
+            //@ts-ignore
+            phone: newUser.phone,
+            //@ts-ignore
+            role: newUser.role,
+         };
+         const accessToken = createToken(
+            tokenPayload,
+            config.jwt_access_secret as string,
+            config.jwt_access_expires_in as string,
+         );
+
+         sendResponse(res, {
+            statusCode: HttpStatusCode.Created,
+            success: true,
+            message: "User has been created successfuly",
+            data: {
+               accessToken
+            }
+         })
+         return;
+      }
+      if (otp) {
+         otp.attempts -= 1;
+         await otp.save();
+      }
+      throw new AppError(
+         HttpStatusCode.BadRequest,
+         'Request Failed',
+         'Invalid OTP! Please try again.',
+      );
+
    })
 }
