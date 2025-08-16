9bc50ea2773afc44c7f9c0d14c51c309e4d00f8b
Jewel-190237 <1902378@ku.ac.bd>
2025-08-16 10:34:48 +0600

    user service added

---
+++ src/app/modules/user/user.service.ts
@@ -0,0 +1,17 @@
+import { HttpStatusCode } from "axios";
+import AppError from "../../errors/AppError";
+import User from "./user.model";
+
+export class UserService {
+   static async createNewUser(payload: any) {
+      const newUser = await User.create(payload)
+
+      if (!newUser) {
+         throw new AppError(
+            HttpStatusCode.BadRequest,
+            'Request Failed',
+            'Failed to create account! Please try again.'
+         )
+      }
+   }
+}
