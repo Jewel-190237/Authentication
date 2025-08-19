ebfd51f50a308305967a52b3d2c1bae0273551f9
Jewel-190237 <1902378@ku.ac.bd>
2025-08-19 20:31:00 +0600

    otp model added

--- src/app/modules/otp/otp.model.ts
+++ src/app/modules/otp/otp.model.ts
@@ -3,6 +3,30 @@ import { TOTP } from "./otp.interface";
 
 const schema = new Schema<TOTP>(
    {
-      
-   }
+      email: {
+         type: String,
+         require: true,
+         trim: true,
+         lowercase: true
+      },
+      otp: {
+         type: String,
+         required: true
+      },
+      action: {
+         type: String,
+         enum: ["signup", "forget_password", "update_profile"],
+         required: true
+      },
+      attempts: {
+         type: Number,
+         default: 3
+      },
+      expireAt: {
+         type: Date,
+         default: Date.now,
+         index: { expires: '2m' }
+      }
+   },
+   { timestamps: true }
 )
