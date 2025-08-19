1feafce95f5ba0519424eb82c26e18afacf7320c
Jewel-190237 <1902378@ku.ac.bd>
2025-08-19 22:24:54 +0600

    user interface and model added

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,22 @@
+import { Types } from 'mongoose';
+export type TUser = {
+    name: string;
+    identifier?: string | undefined | null;
+    phone?: string | undefined | null;
+    password: string;
+    image?: string;
+    role: string;
+    country?: string;
+    city?: string;
+    state?: string;
+    zip_code: number;
+    address?: string;
+    about?: string;
+    permissions?: Types.ObjectId;
+    fcm_token: string[];
+    push_notification_status: boolean;
+    is_deleted: boolean;
+    isModified: (field: string) => boolean;
+};
+
+
